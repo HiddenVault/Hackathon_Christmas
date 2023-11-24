792df3724b1421eabae37b70511a19f3ddcce31c
TheBlackCaverns <144142448+HiddenVault@users.noreply.github.com>
2023-11-24 11:06:41 +0100

    Améliorations

--- christmas.py
+++ christmas.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 link = "christmas_recipes.csv"
 
-df = pd.read_csv(link)
+df = pd.read_csv(link, sep=',', encoding='UTF-8')
 
 image_url= 'https://st4.depositphotos.com/2627021/31189/i/450/depositphotos_311897692-stock-photo-christmas-tree-with-baubles-and.jpg'
 code_html = f"<img src='{image_url}' width='100%'/>"
@@ -12,13 +12,11 @@ st.markdown(code_html, unsafe_allow_html = True)
 st.markdown('<h1 style="color:red;">Christmas Recipes', unsafe_allow_html=True)
 
 # Zone de recherche
-search_zone = st.text_input("Rechercher des ingrédients:")
+search_zone = st.text_input("Search for ingredients :")
 
-st.write("Temps nécessaire à la préparation :")
-selected_time = st.selectbox('Sélectionner une valeur:', ['Toutes'] + df['Time'].unique().tolist())
+selected_time = st.selectbox('Preparation time :', ['All'] + df['Time'].unique().tolist())
 
-st.write("Nombre de couverts :")
-selected_servings = st.selectbox('Sélectionner une valeur:', ['Toutes'] + df['Servings'].unique().tolist())
+selected_servings = st.selectbox('Number of covers :', ['All'] + df['Servings'].unique().tolist())
 
 # Filtrage du dataFrame en fonction des valeurs sélectionnées
 # Filtrage du DataFrame en fonction de la recherche
@@ -26,9 +24,9 @@ selected_servings = st.selectbox('Sélectionner une valeur:', ['Toutes'] + df['S
 # Affichage des résultats
 def dataframe_with_selections(df):
     df = df[df.astype(str).apply(lambda row: row.str.contains(search_zone, case=False).any(), axis=1)]
-    if selected_time != 'Toutes':
+    if selected_time != 'All':
         df = df[df['Time'] == selected_time]
-    if selected_servings != 'Toutes':
+    if selected_servings != 'All':
         df = df[df['Servings'] == selected_servings]
     filtered_df = df.copy()
     filtered_df.insert(0, "Select", False)
@@ -45,32 +43,37 @@ def dataframe_with_selections(df):
 
 selection = dataframe_with_selections(df)
 
+selection['Time'] = selection['Time'].astype(str)
+selection['Ingredients'] = selection['Ingredients'].str.replace(', ', ',\n')
+selection['Instructions'] = selection['Instructions'].str.replace('. ', '.\n')
+
 # Affichage de la recette sélectionnée
 if not selection.empty:
     selected_row = selection.index
+
+    liste_titre = list(selection.loc[selected_row, 'Title'])
+    st.markdown("**Recipe :**")
+    st.text(liste_titre[0])
+
     liste_image = list(selection.loc[selected_row, 'Image'])
     image_url = liste_image[0]
-    st.image(image_url, caption="Photo" )
-    liste_titre = list(selection.loc[selected_row, 'Title'])
-    st.markdown("**Recette :**")
-    st.write(liste_titre[0])
+    st.image(image_url)
 
     liste_temps = list(selection.loc[selected_row, 'Time'])
-    st.markdown("**Temps de préparation :**")
-    st.write(liste_temps[0], 'minutes')
+    st.markdown("**Preparation time in minutes :**")
+    st.text(liste_temps[0])
 
     liste_couverts = list(selection.loc[selected_row, 'Servings'])
-    st.markdown("**Nombre de couverts :**")
-    st.write(liste_couverts[0])
-
+    st.markdown("**Number of covers :**")
+    st.text(liste_couverts[0])
 
     liste_ingrédients = list(selection.loc[selected_row, 'Ingredients'])
-    st.markdown("**Ingrédients :**")
-    st.write(liste_ingrédients[0].replace('[','').replace(']','').replace("'",''))
+    st.markdown("**Ingredients :**")
+    st.text(liste_ingrédients[0].replace('[','').replace(']','').replace("'",''))
 
 
     liste_instructions = list(selection.loc[selected_row, 'Instructions'])
     st.markdown("**Instructions :**")
-    st.write(liste_instructions[0])
+    st.text(liste_instructions[0])
     
     st.markdown('<h3 style="color:red;">Enjoy your meal and Happy Christmas !', unsafe_allow_html=True)
